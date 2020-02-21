902682081f20702bd9c8f54f4d8869c9911ded11
BYUI - Nicolas Carlini <ncarlini@byui.edu>
2020-02-21 09:44:48 -0300

    CSS3 transitions, animations and tranforms

---
+++ js/togglemenu.js
@@ -0,0 +1,12 @@
+
+function toggleNav() {
+    var hamburger = document.getElementById("menu-icon");
+     hamburger.classList.toggle("open");
+   var slidingmenu = document.getElementById("myNav");
+     slidingmenu.classList.toggle("sidenav");
+var slidingmenu = document.getElementById("myNav");
+     slidingmenu.classList.toggle("sidenav_hide");
+var slidingmenu = document.getElementById("heading");
+     slidingmenu.classList.toggle("preserveheader");
+
+}  
